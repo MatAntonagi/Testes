04dbb0aadf23b1ecbfd3cacae6b5894eb4afff31
Matheus Toledo <mtoledoz650@gmail.com>
2025-09-15 20:41:56 -0300

    Agrupando Testes.

--- src/sum.test.ts
+++ src/sum.test.ts
@@ -1,10 +1,18 @@
 import { sum } from "./server";
 
-test("sum of  3 + 7 must be 10", () => {
-  const result = sum(3, 7); // Chama a função sum com 3 e 7
+describe("Sum Function Tests", () => {
+  test("sum of  2 + 2 must be 4", () => {
+    const result = sum(2, 2); // Chama a função sum com 3 e 7
 
-  expect(result).toBe(10); // Verifica se o resultado é 10
-});
+    expect(result).toBe(4); // Verifica se o resultado é 10
+  });
+
+  test("sum of  3 + 7 must be 10", () => {
+    const result = sum(3, 7); // Chama a função sum com 3 e 7
+
+    expect(result).toBe(10); // Verifica se o resultado é 10
+  });
+})
 
 // para rodar esse teste específico, use o comando:
 // npx jest src/sum.test.ts 
