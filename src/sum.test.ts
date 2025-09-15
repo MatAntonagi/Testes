82c320fec07ab6a2a12b0ff0c4fe5fe4df9a5604
Matheus Toledo <mtoledoz650@gmail.com>
2025-09-15 20:32:16 -0300

    Escrevendo o Primeiro Teste.

--- src/sum.test.ts
+++ src/sum.test.ts
@@ -1,5 +1,9 @@
-test("sum", () => {
-  console.log("Test sum running");
+import { sum } from "./server";
+
+test("sum of  3 + 7 must be 10", () => {
+  const result = sum(3, 7); // Chama a função sum com 3 e 7
+
+  expect(result).toBe(10); // Verifica se o resultado é 10
 });
 
 // para rodar esse teste específico, use o comando:
